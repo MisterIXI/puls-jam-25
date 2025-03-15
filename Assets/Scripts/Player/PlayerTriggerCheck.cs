0b4c88b25a4ddbb58e225d9f5072efeab6f9a0fe
SteffWin <steffen.winternheimer@gmail.com>
2025-03-15 21:09:49 +0100

    ADD fish spawn behavior ADD logic to interact with fish swarm

--- Assets/Scripts/Player/PlayerTriggerCheck.cs
+++ Assets/Scripts/Player/PlayerTriggerCheck.cs
@@ -1,13 +1,31 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerTriggerCheck : MonoBehaviour
-{
+{    
+    bool canInteract = false;
     void OnTriggerEnter2D(Collider2D other)
     {
+        if (other.tag == "FishSwarm")
+        {
+            Debug.Log("Triggered");
+            canInteract = true;
+        }
     }
-
 
     void OnTriggerExit2D(Collider2D other)
     {
+        if (other.tag == "FishSwarm")
+        {
+            Debug.Log("Triggered exit");
+            canInteract = false;
+        }
+    }
+    
+    public void InteractWithFishSwarm(InputAction.CallbackContext context)
+    {
+        if(!canInteract) return;
+        if(context.started)
+            Debug.Log("Interacting with fish swarm: Starting mini game");
     }
 }
